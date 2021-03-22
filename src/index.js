c90e30b5d065416d977e36ca015a5aeb1e05cd5f
raiskazi <56422384+raiskazi@users.noreply.github.com>
2021-03-21 22:07:54 -0500

    remove getbutton function

--- src/index.js
+++ src/index.js
@@ -3,19 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-function getButtonText() {
-    return 'Click on me!';
-}
 
 // Create a react component
 const App = () => {
+    const buttonText = 'Click me';
 
     return (
         <div>
-            <label className="label" for="name"> Enter name: </label>
+            <label className="label" htmlFor="name"> Enter name: </label>
             <input id="name" type="text" />
             <button style={{backgroundColor: 'blue', color: 'white'}}>
-                {getButtonText()}
+                {buttonText}
             </button>
         </div>
     );
